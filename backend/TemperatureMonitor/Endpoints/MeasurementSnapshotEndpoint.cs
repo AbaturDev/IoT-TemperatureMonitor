bcae1f0f2a3ccfbdbf8418830187ae4615f00459
quentino-21 <alanomar619@gmail.com>
2025-11-08 00:17:47 +0100

    add enpoint for latest measurement snapshot

--- backend/TemperatureMonitor/Endpoints/MeasurementSnapshotEndpoint.cs
+++ backend/TemperatureMonitor/Endpoints/MeasurementSnapshotEndpoint.cs
@@ -33,6 +33,17 @@ public static class MeasurementSnapshotEndpoint
         .Produces<MeasurementSnapshot>(StatusCodes.Status200OK, "text/event-stream")
         .WithDescription("Returns a continuous stream of live measurement snapshots via Server-Sent Events");
 
+        group.MapGet("/measurements/latest", async (AppDbContext context, CancellationToken ct) =>
+        {
+            var maxTimestamp = await context.MeasurementSnapshots.MaxAsync(x => x.Timestamp, ct);
+            
+            var result = await context.MeasurementSnapshots.FirstOrDefaultAsync(x => x.Timestamp == maxTimestamp, ct);
+        
+            return Results.Ok(result);
+        })
+        .Produces<MeasurementSnapshot>(StatusCodes.Status200OK, "application/json")
+        .WithDescription("Returns a latest measurement snapshot");
+        
         group.MapGet("/measurements", async (AppDbContext context, [FromQuery] DateTimeOffset startDate, [FromQuery] DateTimeOffset endDate, CancellationToken ct) =>
         {
             var result = await context.MeasurementSnapshots
