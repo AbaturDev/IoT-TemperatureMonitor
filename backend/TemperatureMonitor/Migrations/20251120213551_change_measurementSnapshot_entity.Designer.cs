760bcdee21afcba34011f0420ce70fa60e6c5255
quentino-21 <alanomar619@gmail.com>
2025-11-20 22:44:34 +0100

    add migration

--- backend/TemperatureMonitor/Migrations/20251120213551_change_measurementSnapshot_entity.Designer.cs
+++ backend/TemperatureMonitor/Migrations/20251120213551_change_measurementSnapshot_entity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TemperatureMonitor.Database;
 
@@ -11,9 +12,11 @@ using TemperatureMonitor.Database;
 namespace TemperatureMonitor.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251120213551_change_measurementSnapshot_entity")]
+    partial class change_measurementSnapshot_entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/TemperatureMonitor/Migrations/20251120213551_change_measurementSnapshot_entity.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TemperatureMonitor.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_measurementSnapshot_entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Temperature",
+                table: "MeasurementSnapshots",
+                newName: "TemperatureMin");
+
+            migrationBuilder.RenameColumn(
+                name: "Humidity",
+                table: "MeasurementSnapshots",
+                newName: "TemperatureMax");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "MeasurementSnapshots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "HumidityAvg",
+                table: "MeasurementSnapshots",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "TemperatureAvg",
+                table: "MeasurementSnapshots",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "MeasurementSnapshots");
+
+            migrationBuilder.DropColumn(
+                name: "HumidityAvg",
+                table: "MeasurementSnapshots");
+
+            migrationBuilder.DropColumn(
+                name: "TemperatureAvg",
+                table: "MeasurementSnapshots");
+
+            migrationBuilder.RenameColumn(
+                name: "TemperatureMin",
+                table: "MeasurementSnapshots",
+                newName: "Temperature");
+
+            migrationBuilder.RenameColumn(
+                name: "TemperatureMax",
+                table: "MeasurementSnapshots",
+                newName: "Humidity");
+        }
+    }
+}
